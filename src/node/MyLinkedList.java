0fb22f00746771b1d5bc378b1cc360ec5c8e9b01
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-22 23:43:31 -0800

    created variable size for holding the size of data

--- src/node/MyLinkedList.java
+++ src/node/MyLinkedList.java
@@ -4,6 +4,8 @@ import adt.LinkedListADT;
 
 public class MyLinkedList<E> implements LinkedListADT<E> {
 
+    private Node<E> head = null;
+    private int size = 0;
 
     @Override
     public void insert(E data) {
