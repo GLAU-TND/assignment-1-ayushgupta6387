05379b1abd1f7769bcd27bf74f043b8b42b37686
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-22 23:35:07 -0800

    override all the method which are created in ADT

--- src/node/MyLinkedList.java
+++ src/node/MyLinkedList.java
@@ -1,4 +1,20 @@
 package node;
 
-public class MyLinkedList<E> {
+import adt.LinkedListADT;
+
+public class MyLinkedList<E> implements LinkedListADT<E> {
+    @Override
+    public void insert(E data) {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void traverse() {
+
+    }
 }
