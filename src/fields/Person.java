e5a50cc9213ea6919bdc41be8e6882d8e8e6f11f
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-23 10:12:54 -0800

    create long type variable to store the contact number(s)

--- src/fields/Person.java
+++ src/fields/Person.java
@@ -1,8 +1,10 @@
 package fields;
 
+import node.MyLinkedList;
+
 public class Person {
     private String firstName;
     private String lastName;
     private String emailId;
-
+    private MyLinkedList<Long> contactNumber;
 }
