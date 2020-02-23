573bc7489948b3330783982ab01f4de46b143ada
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-23 13:08:38 -0800

    call the method deleteContact to delete the contact

--- src/main/Main.java
+++ src/main/Main.java
@@ -33,6 +33,10 @@ public class Main {
                 case 3:
                     object.searchContact(linkedList);
                     break;
+                case 4:
+                    object.deleteContact(linkedList);
+                    break;
+
             }
         }
     }
