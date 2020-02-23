ad7676a02d90f58860f11ad4261ad3dcdf59be6f
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-22 23:40:45 -0800

    created a constructor containing both the fields

--- src/node/Node.java
+++ src/node/Node.java
@@ -3,4 +3,9 @@ package node;
 public class Node<E> {
     private Node<E> next;
     private E data;
+
+    public Node(Node<E> next, E data) {
+        this.next = next;
+        this.data = data;
+    }
 }
