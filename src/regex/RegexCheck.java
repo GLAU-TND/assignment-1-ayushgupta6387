25854ceb23ca45710c101b629439bd5808e1c18f
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-23 10:33:07 -0800

    created a method to check entered email is correct or not

--- src/regex/RegexCheck.java
+++ src/regex/RegexCheck.java
@@ -1,4 +1,15 @@
 package regex;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexCheck {
+
+    public boolean isEmailCorrect(String emailAddress) {
+        String correctEmail = "[a-zA-Z][a-zA-Z0-9_.]*@[a-zA-Z0-9]+([.][a-zA-Z]+)+";
+        Pattern patternEmail = Pattern.compile(correctEmail);
+        Matcher matcherEmail = patternEmail.matcher(emailAddress);
+        return matcherEmail.matches();
+    }
 }
+
