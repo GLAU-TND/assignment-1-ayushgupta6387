352b23e02fe765ac1561050ca6ad34e3140c5a8b
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-23 12:44:12 -0800

    create an object of MyLinkedList class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,14 @@
 package main;
 
+import fields.Person;
+import node.MyLinkedList;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        boolean loop = false;
+        MyLinkedList<Person> linkedList = new MyLinkedList<>();
     }
 }
