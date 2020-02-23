75da4889e722c98f59ee5245e3d598c749d080ae
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-23 11:08:22 -0800

    create method addDetails to add the name, contact, emailId

--- src/driven/Selector.java
+++ src/driven/Selector.java
@@ -1,7 +1,15 @@
 package driven;
 
+import fields.Person;
+
 import java.util.Scanner;
 
 public class Selector {
     Scanner scanner = new Scanner(System.in);
+    Person person = null;
+
+    public Person addDetails() {
+
+        return person;
+    }
 }
