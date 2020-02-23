379ce3184aad264cdc69383e455116feced023db
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-23 12:46:53 -0800

    ask user for addition of contact(s) or to view the contacts or search contact or delete contact

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,5 +10,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         boolean loop = false;
         MyLinkedList<Person> linkedList = new MyLinkedList<>();
+        while (!loop) {
+            System.out.println("Welcome to Ayush's Contact List App\n" +
+                    "Press 1 to add a new contact\n" +
+                    "Press 2 to view all contacts\n" +
+                    "Press 3 to search for a contact\n" +
+                    "Press 4 to delete a contact\n" +
+                    "Press 5 to exit program ");
+            int choice = scanner.nextInt();
+        }
     }
-}
