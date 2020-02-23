2c0ac7b9ac33a73f2a7b09e0073dc8d731e03d8e
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2020-02-23 10:14:04 -0800

    create getter and setter mehods for all the fields

--- src/fields/Person.java
+++ src/fields/Person.java
@@ -7,4 +7,36 @@ public class Person {
     private String lastName;
     private String emailId;
     private MyLinkedList<Long> contactNumber;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public MyLinkedList<Long> getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(MyLinkedList<Long> contactNumber) {
+        this.contactNumber = contactNumber;
+    }
 }
